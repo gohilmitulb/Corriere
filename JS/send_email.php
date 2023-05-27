dc7f936994efcb8ba77a35553fc6f4f437a14544
Gohil Mitul Bhavesh <119096897+gohilmitulb@users.noreply.github.com>
2023-05-27 12:28:00 +0530

    Update send_email.php

--- JS/send_email.php
+++ JS/send_email.php
@@ -36,13 +36,13 @@ try {
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'corrierepvtltd@gmail.com';                     // SMTP username
-    $mail->Password   = 'xlbmvfymihtxuvpd';                               // SMTP password
+    $mail->Username   = 'your email here';                     // SMTP username
+    $mail->Password   = 'your password here';                               // SMTP password
     $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
-    $mail->setFrom('corrierepvtltd@gmail.com');
+    $mail->setFrom('your password here');
     $mail->addAddress($email);     // Add a recipient
 
   // Content
